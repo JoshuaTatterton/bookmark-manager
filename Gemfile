40a51fae07b84543c0d894b33039a78642eae181
Balint Nemes <squarebe03@gmail.com>
2015-08-10 15:54:57 +0100

    serious issue here

--- Gemfile
+++ Gemfile
@@ -7,7 +7,8 @@ group :test do
   gem 'rspec'
   gem 'capybara'
 
-
 end
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
 
 # gem "rails"
---
+++ app/data_mapper_setup.rb
@@ -0,0 +1,9 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+env = ENV['RACK_ENV'] || 'development'
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+require './app/link'
+DataMapper.finalize
+DataMapper.auto_upgrade!
